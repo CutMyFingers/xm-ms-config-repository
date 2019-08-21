30880d7770299e039fb52e290ecd4cd109147877
CutMyFingers <50481088+CutMyFingers@users.noreply.github.com>
2019-08-21 12:31:15 +0300

    test

--- config/tenants/XM/entity/lep/function/Function$$GET-USER-PROFILES$$tenant.groovy
+++ config/tenants/XM/entity/lep/function/Function$$GET-USER-PROFILES$$tenant.groovy
@@ -2,8 +2,12 @@ import javax.persistence.criteria.CriteriaBuilder
 import javax.persistence.criteria.CriteriaQuery
 import javax.persistence.criteria.Root
 import org.springframework.data.jpa.domain.Specifications
+import org.slf4j.LoggerFactory
 
 entityService = lepContext.services.xmEntity
+
+def log = LoggerFactory.getLogger(getClass())
+log.error('blah')
 
 def users = entityService.findAll(Specifications.where({ Root root, CriteriaQuery query, CriteriaBuilder cb ->
     return cb.equal(root.get('typeKey'), 'USER')
