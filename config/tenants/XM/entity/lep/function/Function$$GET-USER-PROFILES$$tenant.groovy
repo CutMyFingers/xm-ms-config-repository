98fe48e92a8e78245909d5429cba4f18ac678816
CutMyFingers <dark_delphi@ukr.net>
2019-08-21 13:59:14 +0300

    test

---
+++ config/tenants/XM/entity/lep/function/Function$$GET-USER-PROFILES$$tenant.groovy
@@ -0,0 +1,13 @@
+
+import javax.persistence.criteria.CriteriaBuilder
+import javax.persistence.criteria.CriteriaQuery
+import javax.persistence.criteria.Root
+import org.springframework.data.jpa.domain.Specifications
+import org.slf4j.LoggerFactory
+
+entityService = lepContext.services.xmEntity
+
+def log = LoggerFactory.getLogger(getClass())
+log.error("AAAAAAAAAAAAAAAAAAAAAAAAA")
+
+return [: ]
